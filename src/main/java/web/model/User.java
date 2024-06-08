b265b2c288501b7256e13dfc49c65df6c1a21cd6
ZEYTM <zeytm2.0@gmail.com>
2024-06-08 14:50:00 +0300

    save

---
+++ src/main/java/web/model/User.java
@@ -0,0 +1,16 @@
+package web.model;
+
+public class User {
+
+    private int id;
+    private String name;
+    private int age;
+
+    public User() {
+    }
+
+    public User(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
